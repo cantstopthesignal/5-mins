1ae54d65dbc1c683fb9e9c4f363f9313a62222c2
Cant Stop The Signal <cantstopthesignals@gmail.com>
2012-05-06 21:02:29 -0700

    Get oauth2 and demo calendar api request running

--- src/js/main.js
+++ src/js/main.js
@@ -2,34 +2,97 @@
 
 goog.provide('main');
 
+goog.require('goog.asserts');
 goog.require('goog.Uri');
 
 var main = {};
 
-main.handleGapiCalendarCallback = function() {
-  window.console.log("calendar api called back");
-};
-
-main.handleGapiJavascriptClientCallback = function() {
-  window['gapi']['client']['setApiKey'](main.GAPI_API_KEY);
-  window['gapi']['client']['load']('calendar', 'v3',
-      main.handleGapiCalendarCallback);
-};
-
-main.GAPI_JS_CALLBACK_NAME = 'gapi_js_callback';
-
 main.GAPI_API_KEY = 'AIzaSyDh5fbf_pmhJko-6SBua7ptbjnrNl9Jer4';
+main.GAPI_CLIENT_ID = '446611198518.apps.googleusercontent.com';
+main.GAPI_SCOPES = ['https://www.googleapis.com/auth/calendar'];
 
-main.setupGapiJavascriptClient = function() {
-  window[main.GAPI_JS_CALLBACK_NAME] =
-      main.handleGapiJavascriptClientCallback;
+main.GAPI_FULL_AUTH_TIMEOUT = 5000;
+
+main.gapiFullAuthTimerId = null;
+
+main.handleGapiCalendarLoad = function() {
+  window.console.log("main.handleGapiCalendarLoad");
+
+  var request = goog.getObjectByName('gapi.client.request')({
+    path: '/calendar/v3/users/me/calendarList',
+    params: {
+      maxResults: 10
+    }
+  });
+
+  request['execute'](function(resp) {
+    window.console.log(resp);
+  });
+};
+
+main.handleGapiClientLoad = function() {
+  window.console.log('main.handleGapiClientLoad');
+  goog.getObjectByName('gapi.client.setApiKey')(main.GAPI_API_KEY);
+  goog.getObjectByName('gapi.auth.init')(main.handleGapiAuthInit);
+};
+
+main.handleGapiAuthInit = function() {
+  window.console.log('main.handleGapiAuthInit');
+  window.setTimeout(main.checkAuth, 1);
+  main.gapiFullAuthTimerId = window.setTimeout(
+      main.fullAuth, main.GAPI_FULL_AUTH_TIMEOUT);
+};
+
+main.checkAuth = function() {
+  window.console.log('main.checkAuth');
+  goog.getObjectByName('gapi.auth.authorize')({
+    client_id: main.GAPI_CLIENT_ID,
+    scope: main.GAPI_SCOPES,
+    immediate: true
+  }, main.handleAuthResult);
+};
+
+main.fullAuth = function() {
+  window.console.log('main.fullAuth');
+  main.clearFullAuthTimer();
+  goog.getObjectByName('gapi.auth.authorize')({
+    client_id: main.GAPI_CLIENT_ID,
+    scope: main.GAPI_SCOPES,
+    immediate: false
+  }, main.handleAuthResult);
+};
+
+main.clearFullAuthTimer = function() {
+  if (main.gapiFullAuthTimerId) {
+    window.clearTimeout(main.gapiFullAuthTimerId);
+    main.gapiFullAuthTimerId = null;
+  }
+};
+
+main.handleAuthResult = function(authResult) {
+  window.console.log('main.handleAuthResult', authResult);
+  if (!authResult) {
+    // An empty auth result can happen if the user previously authorized
+    // this service but then de-authorized.  Go immediately to full auth
+    // in this case.
+    main.fullAuth();
+    return;
+  }
+  main.clearFullAuthTimer();
+  goog.getObjectByName('gapi.client.load')('calendar', 'v3',
+      main.handleGapiCalendarLoad);
+};
+
+main.loadGapiJavascriptClientAndAuth = function() {
+  var callbackName = 'callback_' + goog.getUid(main.handleGapiClientLoad);
+  goog.exportSymbol(callbackName, main.handleGapiClientLoad);
   var scriptEl = document.createElement("script");
   scriptEl.type = "text/javascript";
   scriptEl.src = "https://apis.google.com/js/client.js?onload=" +
-      main.GAPI_JS_CALLBACK_NAME;
+      callbackName;
   document.body.appendChild(scriptEl);
 };
 
 window.onload = function() {
-  main.setupGapiJavascriptClient();
+  main.loadGapiJavascriptClientAndAuth();
 };
