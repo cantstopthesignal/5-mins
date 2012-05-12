20cea5dc4b0c4e03272ecb7dc393a9567f66874a
Cant Stop The Signal <cantstopthesignals@gmail.com>
2012-05-12 13:06:37 -0700

    First pass at a column based events layout engine.

---
+++ src/js/eventlistlayout.js
@@ -0,0 +1,251 @@
+// Copyright cantstopthesignals@gmail.com
+
+goog.provide('fivemins.EventListLayout');
+goog.provide('fivemins.EventListLayout.Event')
+
+goog.require('goog.array');
+goog.require('goog.asserts');
+goog.require('goog.Disposable');
+goog.require('goog.dom');
+goog.require('goog.math.Rect');
+goog.require('goog.object');
+
+
+/**
+ * @constructor
+ * @extends {goog.Disposable}
+ */
+fivemins.EventListLayout = function() {
+};
+goog.inherits(fivemins.EventListLayout, goog.Disposable);
+
+/** @type {Array.<fivemins.EventListLayout.Event>} */
+fivemins.EventListLayout.prototype.events_;
+
+/** @type {Array.<fivemins.EventListLayout.TimePoint>} */
+fivemins.EventListLayout.prototype.timePoints_;
+
+/** @type {number} */
+fivemins.EventListLayout.prototype.distancePerHour_ = 50;
+
+/** @type {number} */
+fivemins.EventListLayout.prototype.minEventHeight_ = 10;
+
+/** @type {number} */
+fivemins.EventListLayout.prototype.layoutWidth_ = 100;
+
+/** @param {number} width */
+fivemins.EventListLayout.prototype.setLayoutWidth = function(width) {
+  this.layoutWidth_ = width;
+};
+
+/** @param {Array.<fivemins.EventListLayout.Event>} events */
+fivemins.EventListLayout.prototype.setEvents = function(events) {
+  this.events_ = events;
+  goog.array.forEach(this.events_, function(event) {
+    this.registerDisposable(event);
+  }, this);
+  this.events_.sort(function(a, b) {
+    return goog.date.Date.compare(a.startTime, b.startTime);
+  });
+};
+
+fivemins.EventListLayout.prototype.calc = function() {
+  goog.asserts.assert(this.events_);
+
+  this.calcTimePoints_();
+  this.assignEventsToColumns_();
+  this.calcColumnCounts_();
+  this.positionEvents_();
+};
+
+fivemins.EventListLayout.prototype.disposeInternal =
+    function() {
+  delete this.events_;
+  delete this.timePoints_;
+  goog.base(this, 'disposeInternal');
+};
+
+fivemins.EventListLayout.prototype.calcTimePoints_ = function() {
+  var timePointMap = {};
+
+  // Create all relevant time points for the start and end times of all events.
+  goog.array.forEach(this.events_, function(event) {
+    var startPoint = timePointMap[event.startTime];
+    if (!startPoint) {
+      startPoint = new fivemins.EventListLayout.TimePoint(
+          event.startTime);
+      this.registerDisposable(startPoint);
+      timePointMap[startPoint] = startPoint;
+    }
+    startPoint.openEvents.push(event);
+    event.timePoints.push(startPoint);
+    event.startTimePoint = startPoint;
+    var endPoint = timePointMap[event.endTime];
+    if (!endPoint) {
+      endPoint = new fivemins.EventListLayout.TimePoint(
+          event.endTime);
+      this.registerDisposable(endPoint);
+      timePointMap[endPoint] = endPoint;
+    }
+    event.endTimePoint = endPoint;
+  }, this);
+
+  this.timePoints_ = goog.object.getValues(timePointMap);
+  this.timePoints_.sort(function(a, b) {
+    return goog.date.Date.compare(a.time, b.time);
+  });
+
+  var lastTimePoint = null;
+  goog.array.forEach(this.timePoints_, function(timePoint) {
+    if (lastTimePoint) {
+      lastTimePoint.next = timePoint;
+    }
+    lastTimePoint = timePoint;
+  });
+
+  // Populate openEvents with events that span this time point.
+  var timePointIdx = 0;
+  var eventIdx = 0;
+  while (eventIdx < this.events_.length &&
+      timePointIdx < this.timePoints_.length) {
+    var event = this.events_[eventIdx];
+    var timePoint = this.timePoints_[timePointIdx];
+    if (goog.date.Date.compare(event.startTime, timePoint.time) >= 0) {
+      timePointIdx += 1;
+    } else if (goog.date.Date.compare(event.endTime, timePoint.time) < 0) {
+      eventIdx += 1;
+    } else {
+      timePoint.openEvents.push(event);
+      event.timePoints.push(timePoint);
+      eventIdx += 1;
+    }
+  }
+};
+
+fivemins.EventListLayout.prototype.assignEventsToColumns_ =
+    function() {
+  var eventsByDuration = goog.array.clone(this.events_);
+  eventsByDuration.sort(function(a, b) {
+    var aDuration = a.endTime.getTime() - a.startTime.getTime();
+    var bDuration = b.endTime.getTime() - b.startTime.getTime();
+    return a - b;
+  });
+  goog.array.forEach(eventsByDuration, function(event) {
+    var usedColumns = {};
+    goog.array.forEach(event.timePoints, function(timePoint) {
+      goog.array.forEach(timePoint.openEvents, function(neighborEvent) {
+        if (neighborEvent.columnAssigned) {
+          var column = neighborEvent.column;
+          usedColumns[column] = (usedColumns[column] || 0) + 1;
+        }
+      });
+    });
+    var column = 0;
+    while (usedColumns[column]) {
+      column++;
+    }
+    event.column = column;
+    event.columnAssigned = true;
+  });
+};
+
+fivemins.EventListLayout.prototype.calcColumnCounts_ =
+    function() {
+  goog.array.forEach(this.events_, function(event) {
+    event.columnCount = event.column + 1;
+  });
+  goog.array.forEach(this.timePoints_, function(timePoint) {
+    timePoint.columnCount = 0;
+  });
+  var done = false;
+  while (!done) {
+    done = true;
+    goog.array.forEach(this.events_, function(event) {
+      goog.array.forEach(event.timePoints, function(timePoint) {
+        var columnCount = Math.max(event.columnCount,
+            timePoint.columnCount);
+        if (columnCount != event.columnCount ||
+            columnCount != timePoint.columnCount) {
+          event.columnCount = timePoint.columnCount = columnCount;
+          done = false;
+        }
+      });
+    });
+  }
+};
+
+fivemins.EventListLayout.prototype.positionEvents_ =
+    function() {
+  var yPos = 0;
+  goog.array.forEach(this.timePoints_, function(timePoint) {
+    timePoint.yPos = yPos;
+    if (!timePoint.next) {
+      return;
+    }
+    var timeInterval = timePoint.next.getTime() - timePoint.getTime();
+    var yInterval = Math.ceil(this.distancePerHour_ *
+        (timeInterval / 1000 / 60 / 60));
+    yPos += yInterval;
+  }, this);
+  goog.array.forEach(this.events_, function(event) {
+    var columnWidth = Math.ceil(this.layoutWidth_ / event.columnCount);
+    var x = columnWidth * event.column;
+    var height = event.endTimePoint.yPos - event.startTimePoint.yPos;
+    event.rect = new goog.math.Rect(x, event.startTimePoint.yPos,
+        columnWidth, height);
+  }, this);
+};
+
+/**
+ * @constructor
+ * @extends {goog.Disposable}
+ */
+fivemins.EventListLayout.TimePoint = function(time) {
+  this.time = time;
+  this.next = null;
+  this.yPos = null;
+  this.openEvents = [];
+  this.columnCount = null;
+};
+goog.inherits(fivemins.EventListLayout.TimePoint, goog.Disposable);
+
+fivemins.EventListLayout.TimePoint.prototype.disposeInternal =
+    function() {
+  delete this.openEvents;
+  delete this.next;
+  goog.base(this, 'disposeInternal');
+};
+
+fivemins.EventListLayout.TimePoint.prototype.toString = function() {
+  return this.time.toString();
+};
+
+fivemins.EventListLayout.TimePoint.prototype.getTime = function() {
+  return this.time.getTime();
+};
+
+/**
+ * @constructor
+ * @extends {goog.Disposable}
+ */
+fivemins.EventListLayout.Event = function(startTime, endTime) {
+  this.startTime = startTime;
+  this.endTime = endTime;
+  this.column = null;
+  this.columnAssigned = false;
+  this.columnCount = null;
+  this.rect = null;
+  this.timePoints = [];
+  this.startTimePoint = null;
+  this.endTimePoint = null;
+};
+goog.inherits(fivemins.EventListLayout.Event, goog.Disposable);
+
+fivemins.EventListLayout.Event.prototype.disposeInternal =
+    function() {
+  delete this.timePoints;
+  delete this.startTimePoint;
+  delete this.endTimePoint;
+  goog.base(this, 'disposeInternal');
+};
---
+++ src/js/eventlistlayoutdemo.js
@@ -0,0 +1,104 @@
+// Copyright cantstopthesignals@gmail.com
+
+goog.provide('fivemins.EventListLayoutDemo');
+
+goog.require('fivemins.EventListLayout');
+goog.require('goog.array');
+goog.require('goog.asserts');
+goog.require('goog.date.DateTime');
+goog.require('goog.dom');
+goog.require('goog.events.EventTarget');
+goog.require('goog.style');
+
+/**
+ * @constructor
+ */
+fivemins.EventListLayoutDemo = function() {
+  this.events_ = [];
+
+  this.el_ = document.createElement('div');
+  this.el_.className = 'event-area';
+  document.body.appendChild(this.el_);
+
+  this.eventContainerEl_ = document.createElement('div');
+  this.eventContainerEl_.className = 'event-container';
+  this.el_.appendChild(this.eventContainerEl_);
+
+  this.now_ = new goog.date.DateTime();
+};
+
+fivemins.EventListLayoutDemo.prototype.start = function() {
+  this.createSomeEvents_();
+};
+
+fivemins.EventListLayoutDemo.prototype.createSomeEvents_ = function() {
+  this.events_.push(this.createEvent_('Event 1', 0, 1));
+  this.events_.push(this.createEvent_('Event 2', 3.2, 1));
+  this.events_.push(this.createEvent_('Event 3', 1.1, 2));
+  this.events_.push(this.createEvent_('Event 4', 1.1, 8));
+  this.events_.push(this.createEvent_('Event 5', 5, 3));
+
+  goog.array.forEach(this.events_, function(event) {
+    event.render(this.eventContainerEl_);
+  }, this);
+
+  this.layout_();
+};
+
+fivemins.EventListLayoutDemo.prototype.layout_ = function() {
+  var layoutEvents = goog.array.map(this.events_, function(event) {
+    var layoutEvent = new fivemins.EventListLayout.Event(
+        event.startTime, event.endTime);
+    layoutEvent.demoEvent = event;
+    return layoutEvent;
+  }, this);
+  var layout = new fivemins.EventListLayout();
+  layout.setLayoutWidth(500);
+  layout.setEvents(layoutEvents);
+  layout.calc();
+  var eventContainerHeight = 0;
+  goog.array.forEach(layoutEvents, function(layoutEvent) {
+    var event = layoutEvent.demoEvent;
+    window.console.log(event.name, 'column', layoutEvent.column, 'columnCount',
+        layoutEvent.columnCount, 'rect', layoutEvent.rect.toString());
+    event.setRect(layoutEvent.rect);
+    eventContainerHeight = Math.max(layoutEvent.rect.top +
+        layoutEvent.rect.height, eventContainerHeight);
+  }, this);
+  goog.dispose(layout);
+  goog.style.setHeight(this.eventContainerEl_, eventContainerHeight);
+};
+
+fivemins.EventListLayoutDemo.prototype.createEvent_ = function(
+    name, startOffsetHours, durationHours) {
+  var startTime = this.now_.clone();
+  var startOffsetSeconds = Math.round(startOffsetHours * 60 * 60);
+  startTime.add(new goog.date.Interval(goog.date.Interval.SECONDS,
+      startOffsetSeconds));
+  var endTime = startTime.clone();
+  var durationSeconds = Math.round(durationHours * 60 * 60);
+  endTime.add(new goog.date.Interval(goog.date.Interval.SECONDS,
+      durationSeconds));
+  return new fivemins.EventListLayoutDemo.Event(name, startTime, endTime);
+};
+
+
+/** @constructor */
+fivemins.EventListLayoutDemo.Event = function(name, startTime, endTime) {
+  this.name = name;
+  this.startTime = startTime;
+  this.endTime = endTime;
+  this.el_ = document.createElement('div');
+  this.el_.className = 'event';
+  this.el_.appendChild(document.createTextNode(name));
+};
+
+fivemins.EventListLayoutDemo.Event.prototype.render = function(parentEl) {
+  parentEl.appendChild(this.el_);
+};
+
+/** @param {goog.math.Rect} rect */
+fivemins.EventListLayoutDemo.Event.prototype.setRect = function(rect) {
+  goog.style.setPosition(this.el_, rect.left, rect.top);
+  goog.style.setBorderBoxSize(this.el_, rect.getSize());
+};
