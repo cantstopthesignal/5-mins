9c59c1be5171195d03f0b7c14dd9dc5d20809650
Cant Stop The Signal <cantstopthesignals@gmail.com>
2021-05-14 16:48:13 -0700

    web: cache last manifest in IndexedDB for better change detection

--- web/src/main/javascript/serviceworker.js
+++ web/src/main/javascript/serviceworker.js
@@ -3,32 +3,45 @@
 goog.provide('five.ServiceWorker');
 
 goog.require('goog.asserts');
+goog.require('goog.db');
+goog.require('goog.db.Transaction');
 goog.require('goog.events');
 goog.require('goog.events.EventHandler');
 goog.require('goog.events.EventTarget');
 goog.require('goog.events.EventType');
 goog.require('goog.log');
 
-
 /**
  * @constructor
  * @extends {goog.events.EventTarget}
  */
 five.ServiceWorker = function() {
+  /** @type {goog.async.Deferred} */
+  this.dbDeferred_ = new goog.async.Deferred();
+
+  /** @type {goog.async.Deferred} */
+  this.lastManifestDeferred_ = new goog.async.Deferred();
+
   /** @type {goog.events.EventHandler} */
   this.eventHandler_ = new goog.events.EventHandler(this);
   this.registerDisposable(this.eventHandler_);
 };
 goog.inherits(five.ServiceWorker, goog.events.EventTarget);
 
-/** @type {goog.log.Logger} */
-five.ServiceWorker.prototype.logger_ = goog.log.getLogger('five.App');
-
 /** @type {string} */
 five.ServiceWorker.CACHE_NAME = "default-cache";
 
 /** @type {string} */
-five.ServiceWorker.prototype.lastManifest_;
+five.ServiceWorker.LAST_MANIFEST_DB_NAME = 'lastManifest';
+
+/** @type {string} */
+five.ServiceWorker.LAST_MANIFEST_DB_STORE_NAME = 'store';
+
+/** @type {string} */
+five.ServiceWorker.LAST_MANIFEST_DB_VALUE_KEY = 'value';
+
+/** @type {goog.log.Logger} */
+five.ServiceWorker.prototype.logger_ = goog.log.getLogger('five.App');
 
 five.ServiceWorker.prototype.start = function() {
   this.logger_.info('start');
@@ -40,6 +53,41 @@ five.ServiceWorker.prototype.start = function() {
       listen(self, goog.events.EventType.MESSAGE, this.handleMessage_);
 };
 
+five.ServiceWorker.prototype.loadDbAndLastManifest_ = function() {
+  var dbDeferred = goog.db.openDatabase(
+    five.ServiceWorker.LAST_MANIFEST_DB_NAME, 1,
+    function(ev, db, tx) {
+      db.createObjectStore(five.ServiceWorker.LAST_MANIFEST_DB_STORE_NAME);
+    });
+  dbDeferred.addCallbacks(function(db) {
+      var getTx = db.createTransaction([five.ServiceWorker.LAST_MANIFEST_DB_STORE_NAME]);
+      var request = getTx.objectStore(five.ServiceWorker.LAST_MANIFEST_DB_STORE_NAME).
+        get(five.ServiceWorker.LAST_MANIFEST_DB_VALUE_KEY);
+      request.addCallbacks(function(result) {
+          this.lastManifestDeferred_.callback(result || null);
+        }, function(err) {
+          this.logger_.severe('Failed to read last manifest value: ' + err, err);
+        }, this);
+    }, function(err) {
+      this.logger_.severe('Failed to open last manifest database: ' + err, err);
+    }, this);
+
+  dbDeferred.chainDeferred(goog.asserts.assertObject(this.dbDeferred_));
+};
+
+five.ServiceWorker.prototype.saveLastManifest_ = function(lastManifestData) {
+  this.dbDeferred_.addCallback(function(db) {
+    var putTx = db.createTransaction(
+      [five.ServiceWorker.LAST_MANIFEST_DB_STORE_NAME],
+      goog.db.Transaction.TransactionMode.READ_WRITE);
+    var store = putTx.objectStore(five.ServiceWorker.LAST_MANIFEST_DB_STORE_NAME);
+    store.put(lastManifestData, five.ServiceWorker.LAST_MANIFEST_DB_VALUE_KEY);
+    putTx.wait().addErrback(function(err) {
+      this.logger_.severe('Failed to store last manifest value: ' + err, err);
+    }, this);
+  }, this);
+};
+
 five.ServiceWorker.prototype.handleInstall_ = function(e) {
   this.logger_.info('handleInstall_');
 
@@ -48,6 +96,8 @@ five.ServiceWorker.prototype.handleInstall_ = function(e) {
 
 five.ServiceWorker.prototype.handleActivate_ = function(e) {
   this.logger_.info('handleActivate_');
+
+  this.loadDbAndLastManifest_();
 
   var event = e.getBrowserEvent();
   event.waitUntil(self['clients'].claim());
@@ -85,27 +135,35 @@ five.ServiceWorker.prototype.handleMessage_ = function(e) {
   var event = e.getBrowserEvent();
 
   if (event.data['command'] == 'checkAppUpdateAvailable') {
-    this.logger_.info('handleMessage_: checkAppUpdateAvailable');
-
     fetch('/manifest.txt')
       .then(response => response.text())
-      .then(function(data) {
-        if (this.lastManifest_ == null) {
-          this.logger_.info('handleMessage_: checkAppUpdateAvailable: first manifest');
-          this.lastManifest_ = data;
-          return;
-        }
-        if (this.lastManifest_ == data) {
-         this.logger_.info('handleMessage_: checkAppUpdateAvailable: manifest matches');
-          return;
-        }
+      .then(function(manifestData) {
+        this.lastManifestDeferred_.addCallback(function(lastManifestData) {
+          if (manifestData && lastManifestData != manifestData) {
+            this.saveLastManifest_(manifestData);
+          }
 
-        this.logger_.info('handleMessage_: checkAppUpdateAvailable: manifest updated!');
-        this.lastManifest_ = data;
+          if (lastManifestData == null) {
+            this.logger_.info('handleMessage_: checkAppUpdateAvailable: first manifest');
+            return;
+          }
 
-        event.source.postMessage({'command': 'updateAvailable'});
+          if (lastManifestData == manifestData) {
+            this.logger_.info('handleMessage_: checkAppUpdateAvailable: manifest matches');
+            return;
+          }
 
-        self['caches'].delete(five.ServiceWorker.CACHE_NAME);
+          this.logger_.info('handleMessage_: checkAppUpdateAvailable: manifest updated!');
+          event.source.postMessage({'command': 'updateAvailable'});
+
+          self['caches'].delete(five.ServiceWorker.CACHE_NAME);
+        }, this);
+
+        this.lastManifestDeferred_ = goog.async.Deferred.succeed(manifestData);
+      }.bind(this))
+      .catch(function(err) {
+        this.logger_.info('handleMessage_: checkAppUpdateAvailable: error fetching manifest: '
+            + err, err);
       }.bind(this));
   }
 };
